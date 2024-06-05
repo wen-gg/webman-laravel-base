65d4fdbfaaf07443d4433e817b5f30fcca3e0f22
Mosquito <zwj1206_hi@163.com>
2024-06-05 14:06:04 +0800

    优化配置复用

--- src/Triats/ConfigTrait.php
+++ src/Triats/ConfigTrait.php
@@ -12,23 +12,51 @@ trait ConfigTrait
      * 配置格式化
      * @return array|string
      * @author mosquito <zwj1206_hi@163.com>
+     * @deprecated v1.0.1
      */
     protected static function configFormat(array $arr, string $key = null, bool $transform = false)
     {
+        return static::configFormatNew($arr, $key, $transform === true ? 'key' : null, $transform === true ? 'value' : null);
+    }
+
+    /**
+     * 配置格式化
+     * @param array $arr
+     * @param mixed $key
+     * @param string|null $keyName
+     * @param string|null $valueName
+     * @author mosquito <zwj1206_hi@163.com>
+     */
+    protected static function configFormatNew(array $arr, mixed $key = null, string $keyName = null, string $valueName = null)
+    {
         $result = null;
         if (is_null($key)) {
-            if ($transform === true) {
+            if ($keyName !== null && $valueName !== null) {
                 $result = [];
                 foreach ($arr as $k => $v) {
                     $result[] = [
-                        'key' => $k,
-                        'value' => $v,
+                        $keyName => $k,
+                        $valueName => $v,
                     ];
                 }
             } else {
                 $result = $arr;
             }
-        } else {
+        } elseif (is_array($key)) {
+            $result = [];
+            if ($keyName !== null && $valueName !== null) {
+                foreach ($key as $k) {
+                    $result[] = [
+                        $keyName => $k,
+                        $valueName => $arr[$k],
+                    ];
+                }
+            } else {
+                foreach ($key as $k) {
+                    $result[$k] = $arr[$k];
+                }
+            }
+        } elseif (is_int($key) || is_string($key)) {
             $result = $arr[$key];
         }
         return $result;
